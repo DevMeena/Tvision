ac779ebd5b09753c6960eac5b6f5aabf05f1768f
DevMeena <68868864+DevMeena@users.noreply.github.com>
2022-07-11 21:08:50 +0530

    fixed watchlist deletion

--- src/components/list-content/list-content.js
+++ src/components/list-content/list-content.js
@@ -1,54 +1,103 @@
-import React from 'react'
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Picture } from 'react-responsive-picture';
 import { Button } from '@material-ui/core';
-import { doc, updateDoc, arrayRemove } from "firebase/firestore"
-import { db } from '../../firebase'
+import { doc, updateDoc, arrayRemove } from 'firebase/firestore';
+import { db } from '../../firebase';
 import { useAuth } from '../../contexts/AuthContext';
 import { useHistory } from 'react-router';
 
 const ListContent = (media) => {
+  console.log(media);
 
-      const delObj = {
-        media_id: media.media.media_id,
-        media_rating: media.media.media_rating,
-        media_title: media.media.media_title,
-        media_type: media.media.media_type,
-        poster_url: media.media.poster_url,
-        release_year: media.media.release_year
-      }
+  const delObj = {
+    media_id: media.media.media_id,
+    media_rating: media.media.media_rating,
+    media_title: media.media.media_title,
+    media_type: media.media.media_type,
+    poster_url: media.media.poster_url,
+    release_year: media.media.release_year,
+  };
 
-      const {currentUser} = useAuth()
-      const history = useHistory()
+  const { currentUser } = useAuth();
+  const history = useHistory();
 
+  const handleViewMedia = () => {
+    history.push(
+      '/content/' + media.media.media_type + '/' + media.media.media_id
+    );
+  };
 
+  const handleRemovefromWatchlist = async (e) => {
+    e.preventDefault();
+    media.onChange(e.target.value);
+    console.log(currentUser.uid);
+    await updateDoc(doc(db, 'users', currentUser.uid), {
+      myList: arrayRemove(delObj),
+    });
+  };
 
-      const handleViewMedia = () => {
-        history.push("/content/" + media.media.media_type + "/" + media.media.media_id)
-      }
+  return (
+    <div className='table-texture'>
+      <br />
 
-      const handleRemovefromWatchlist = async () => {
-        console.log(currentUser.uid);
-        await updateDoc(doc(db,"users",currentUser.uid), {myList: arrayRemove(delObj)})
-      }
+      <Grid
+        alignItems='center'
+        justifyContent='center'
+        className='table-grid-holder'
+        container
+      >
+        <Grid item className='table-movie-poster' lg={2} md={2} sm={12} xs={12}>
+          {' '}
+          <Picture
+            className='table-movie-poster-img'
+            src={media.media.poster_url}
+          />{' '}
+        </Grid>
+        <Grid item className='table-movie-name' lg={5} md={5} sm={12} xs={12}>
+          {' '}
+          <p>{media.media.media_title}</p>{' '}
+        </Grid>
+        <Grid item className='table-movie-year' lg={1} md={1} sm={12} xs={12}>
+          {' '}
+          <p>{media.media.release_year}</p>{' '}
+        </Grid>
+        <Grid item className='table-movie-rating' lg={1} md={1} sm={12} xs={12}>
+          {' '}
+          <p>{media.media.media_rating} ⭐ </p>{' '}
+        </Grid>
+        <Grid
+          item
+          className='table-movie-view'
+          onClick={handleViewMedia}
+          lg={1}
+          md={1}
+          sm={12}
+          xs={12}
+        >
+          {' '}
+          <Button variant='text' color='primary'>
+            {' '}
+            View{' '}
+          </Button>{' '}
+        </Grid>
+        <Grid item className='table-movie-remove' lg={1} md={1} sm={12} xs={12}>
+          <Button
+            variant='text'
+            type='submit'
+            onClick={handleRemovefromWatchlist}
+            value={media.media.media_id}
+            color='secondary'
+          >
+            Remove
+          </Button>
+        </Grid>
+      </Grid>
 
-    return (
-        <div className="table-texture" >
-            <br/>
+      <br />
+      <hr />
+    </div>
+  );
+};
 
-            <Grid alignItems="center" justifyContent="center" className="table-grid-holder" container>
-            <Grid item className="table-movie-poster" lg={2} md={2} sm={12} xs={12}> <Picture className="table-movie-poster-img" src={media.media.poster_url} /> </Grid>
-            <Grid item className="table-movie-name" lg={5} md={5} sm={12} xs={12}> <p>{media.media.media_title}</p> </Grid>
-            <Grid item className="table-movie-year" lg={1} md={1} sm={12} xs={12}> <p>{media.media.release_year}</p> </Grid>
-            <Grid item className="table-movie-rating" lg={1} md={1} sm={12} xs={12}> <p>{media.media.media_rating} ⭐ </p> </Grid>
-            <Grid item className="table-movie-view" onClick={handleViewMedia} lg={1} md={1} sm={12} xs={12}> <Button variant='text' color="primary" > View </Button> </Grid>
-            <Grid item className="table-movie-remove" onClick={handleRemovefromWatchlist} lg={1} md={1} sm={12} xs={12}> <Button variant='text' color="secondary" > Remove </Button>  </Grid>
-            </Grid>
-
-            <br/>
-            <hr/>
-        </div>
-    )
-}
-
-export default ListContent
+export default ListContent;
--- src/components/watchlist/watchlist.js
+++ src/components/watchlist/watchlist.js
@@ -1,68 +1,70 @@
-import React from 'react'
+import React from 'react';
 import { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
-import ListContent from '../list-content/list-content'
-import { useAuth } from '../../contexts/AuthContext'
-import { collection, doc, getDoc } from "firebase/firestore"
-import { db, } from '../../firebase'
-
+import ListContent from '../list-content/list-content';
+import { useAuth } from '../../contexts/AuthContext';
+import { collection, doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 const Watchlist = () => {
+  const [watchlistMovies, setwatchlistMovies] = useState([]);
+  const [error, setError] = useState(false);
+  const [del, setDel] = useState('');
 
-    const [watchlistMovies, setwatchlistMovies] = useState([]);
-    const [error, setError] = useState(false)
+  const { currentUser } = useAuth();
+  // const usersCollectionRef = collection(db,"users")
+  const docRef = doc(db, 'users', currentUser.uid);
 
-
-    const {currentUser} = useAuth()
-    // const usersCollectionRef = collection(db,"users")
-    const docRef = doc(db,"users",currentUser.uid)
-    
-    const getWatchListMovies = async () => {
-        const data = await getDoc(docRef)
-        if (data.exists()) {
-            setwatchlistMovies([...data.data().myList])
-            if(!watchlistMovies.length){
-                setError(true)
-                console.log(watchlistMovies.length);
-            } else {
-                setError(false)
-            }
-        } else {
-            console.log("empty");
-        }
+  const getWatchListMovies = async () => {
+    const data = await getDoc(docRef);
+    if (data.exists()) {
+      setwatchlistMovies([...data.data().myList]);
+      if (!watchlistMovies.length) {
+        setError(true);
+        console.log(watchlistMovies.length);
+      } else {
+        setError(false);
+      }
+    } else {
+      console.log('empty');
     }
+  };
 
-    useEffect(() => {
-        getWatchListMovies();
-    }, [watchlistMovies])
+  useEffect(() => {
+    getWatchListMovies();
+    return () => {
+      setwatchlistMovies([]); // unmounting
+      setDel('');
+    };
+  }, [error, del]);
 
-    return (
-        <div className="content-page-area">
-            <h1 className="trending-text">  My Watchlist </h1>
-           
-            
-            <Container className="watchlist-container">
-            <hr/>
+  return (
+    <div className='content-page-area'>
+      <h1 className='trending-text'> My Watchlist </h1>
 
-            { error && <div className='error-division'><h1 className="error-text" > No Media to Watch </h1></div>}
+      <Container className='watchlist-container'>
+        <hr />
 
+        {error && (
+          <div className='error-division'>
+            <h1 className='error-text'> No Media to Watch </h1>
+          </div>
+        )}
 
-            {watchlistMovies && watchlistMovies.map((item) => (
+        {watchlistMovies &&
+          watchlistMovies.map((item) => (
+            <ListContent
+              key={item.media_id}
+              media={item}
+              onChange={(value) => setDel(value)}
+            />
+          ))}
+      </Container>
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+};
 
-                <ListContent
-                key={item.media_id}
-                media={item}
-                />
-            ))}
-
-
-            
-            </Container>
-            <br/>
-            <br/>
-            <br/>
-        </div>
-    )
-}
-
-export default Watchlist
+export default Watchlist;
